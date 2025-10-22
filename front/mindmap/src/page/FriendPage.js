3d455b6de949f4874ab216322656d01e59e858c5
JangSeung <164428037+Jangseun@users.noreply.github.com>
2025-10-22 15:55:21 +0900

    껍대기 대충 완성

--- front/mindmap/src/page/FriendPage.js
+++ front/mindmap/src/page/FriendPage.js
@@ -1,17 +1,49 @@
-import "./HomePage.css"
-import { useNavigate } from 'react-router-dom';
+import "./FriendPage.css"
 
-import Header from "../component/Header"
+import Header from "../component/Header";
+import Friends from "../component/Friends";
 
-const FriendPage = () => {
+import { Search } from "lucide-react";
+
+const Friend = () => {
     return(
-        <div className="wrap_ho">
+        <div className="wrap_f">
             <Header />
-            <div className='text_wrap'>
-                <h1 className='main_text'>FriendPage</h1>
+            <div className="info">
+                <div className='text_wrap_f'>
+                    <h1 className='main_text_f'>Friend</h1>
+                    <div>
+                        <input type="s_friend" placeholder="TRY SERCHING FOR NEW FRIEND"/>
+                        <Search className="search" />  
+                    </div>
+                    <p className="sub_text_f">(12)</p>             
+                </div>
+                <div className="people_wrap">
+                    <Friends />
+                    <Friends />
+                    <Friends />
+                    <Friends />
+                </div>
+                <p className="sub_text_f">OTHER PEOPLE</p>
+                <div className="opeople_wrap">
+                    <Friends />
+                    <Friends />
+                    <Friends />
+                    <Friends />
+                    <Friends />
+                    <Friends />
+                    <Friends />
+                    <Friends />
+                    <Friends />
+                    <Friends />
+                    <Friends />
+                    
+                </div>
+        
             </div>
+            
         </div>
     );
 }
 
-export default FriendPage;
+export default Friend;
--- front/mindmap/src/page/MemoPage.js
+++ front/mindmap/src/page/MemoPage.js
@@ -1,15 +1,29 @@
-import "./HomePage.css"
-import { useNavigate } from 'react-router-dom';
+import "./MemoPage.css"
 
-import Header from "../component/Header"
+import Header from "../component/Header";
 
 const MemoPage = () => {
     return(
         <div className="wrap_ho">
             <Header />
-            <div className='text_wrap'>
-                <h1 className='main_text'>MemoPage</h1>
+            <div className="info">
+                <div className='text_wrap_ho'>
+                    <h1 className='main_text_ho'>Memo</h1>
+                </div>
+                <div className="resent_wrap">
+                    <div className="memo_wrap">
+                        <p>memo1</p>
+                        <p>memo2</p>
+                        <p>memo3</p>
+                        <p>memo4</p>
+                        <p>memo5</p>
+                        <p>memo6</p>
+                        <p>memo7</p>
+                    </div>
+                    
+                </div>
             </div>
+            
         </div>
     );
 }
